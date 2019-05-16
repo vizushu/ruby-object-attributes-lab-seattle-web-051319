2792def1a543d3777fc8a33669daeb3ac64b09e2
Renier De La Cruz <me@renierdelacruz.com>
2019-05-16 00:17:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,19 @@
 class Person
-    def job = (job)
-        @job = job
-    end
-    def name = (name)
-        @name = name
-    end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
+  
 end
